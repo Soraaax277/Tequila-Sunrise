a7968e72eedc2ff2dd537dfb13aae5b9bce44d03
Soraaax277 <marcaceedu@gmail.com>
2024-09-21 16:10:48 +0800

    xd

--- testPage.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.room').forEach(room => {
-    room.addEventListener('click', function() {
-        const selectedRoom = this.getAttribute('data-room');
-        document.querySelector('#room').value = selectedRoom.toLowerCase().replace(' ', '-');
-        alert(`You selected the ${selectedRoom}`);
-    });
-});
