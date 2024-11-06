f66764643d598dd4b0df5ce2cb2f66d5190c151b
Soraaax277 <marcaceedu@gmail.com>
2024-11-07 02:02:19 +0800

    updated server and app js

--- tequila-moonrise/src/server.js
+++ tequila-moonrise/src/server.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose'); 
 const app = express();
 const port = 3001;
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Tequila Moonrise API');
+
+const mongoURI = 'mongodb://localhost:27017/tequilaSunrise';
+
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch(err => {
+    console.error('MongoDB connection error:', err);
 });
 
+app.get('/', (req, res) => {
+    res.send('Tequila Sunrise API');
+});
+
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
