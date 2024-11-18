e18aba93c26f8491e17631549893e9a3958ffaed
Soraaax277 <marcaceedu@gmail.com>
2024-11-18 12:30:45 +0800

    fixed footer

--- tequila-moonrise/src/Footer.js
+++ tequila-moonrise/src/Footer.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import './Footer.css'; // Make sure to create this CSS file as well
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footercontainer">
-        <p className="footer-text">© 2023 Tequila Moonrise. All Rights Reserved.</p>
+      <div className="container">
+        <p className="footer-text">© 2023 Your Hotel Name. All rights reserved.</p>
         <ul className="footer-links">
           <li><a href="/about">About Us</a></li>
           <li><a href="/contact">Contact</a></li>
-          <li><a href="/terms">Terms of Service</a></li>
           <li><a href="/privacy">Privacy Policy</a></li>
         </ul>
+        <div className="social-media">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="/path/to/facebook-logo.png" alt="Facebook" />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src="/path/to/twitter-logo.png" alt="Twitter" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src="/path/to/instagram-logo.png" alt="Instagram" />
+          </a>
+          {/* Add more social media links as needed */}
+        </div>
       </div>
     </footer>
   );
