d7475461842ad08476ab1610e8b9eeb2fcc058ec
Soraaax277 <marcaceedu@gmail.com>
2024-11-18 10:39:00 +0800

    fixed navbar size

--- tequila-moonrise/src/components/js_functions/Navbar.js
+++ tequila-moonrise/src/components/js_functions/Navbar.js
@@ -13,122 +13,123 @@ import '../../css/Nav.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-
 export default function Navbar() {
     const currentWindow = useLocation();
     const navigate = useNavigate();
-
-    // Set up the page transition with navigation to /hotel
     const { isTransitioning, triggerTransition, slideClass, videoClass } = usePageTransition();
-    
-    const handleNavLinkClick = (e, path) => {
-        e.preventDefault();  // Prevent default navigation
-        triggerTransition(() => navigate(path));  // Trigger transition, then navigate
+
+    const handleNavLinkClick = (e, path, isBooking = false) => {
+        e.preventDefault();
+        if (isBooking) {
+            triggerTransition(() => navigate(path));
+        } else {
+            navigate(path);
+        }
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark flex-column">
-          <div className="container-fluid">
-            <div className='d-flex justify-content-between align-items-center w-100'>
-            <a className="navbar-brand" href="#">
-              <img
-                src={Logo}
-                alt="Logo of a satellite with stars"
-                height="50"
-                width="50"
-              />
-              TEQUILA MOONRISE
-            </a>
-            {currentWindow.pathname !== '/hotel' && (
-                    <NavLink 
-                    to="/hotel" 
-                    onClick={(e) => handleNavLinkClick(e, '/HotelReserve')}  // Use the transition
-                    className="btn btn-outline-light" 
-                    activeClassName="active"
-                  >
-                    BOOK NOW
-                  </NavLink>)}
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" onClick={() => navigate('/')} style={{ cursor: 'pointer' }}>
+                    <img
+                        src={Logo}
+                        alt="Logo of a satellite with stars"
+                    />
+                    TEQUILA MOONRISE
+                </a>
+                <div className="collapse navbar-collapse justify-content-end">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink 
+                                to="/" 
+                                className="nav-link" 
+                                activeClassName="active"
+                                onClick={(e) => handleNavLinkClick(e, '/')}
+                            >
+                                HOME
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink 
+                                to="/about" 
+                                className="nav-link" 
+                                activeClassName="active"
+                                onClick={(e) => handleNavLinkClick(e, '/about')}
+                            >
+                                ABOUT
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink 
+                                to="/stay" 
+                                className="nav-link" 
+                                activeClassName="active"
+                                onClick={(e) => handleNavLinkClick(e, '/stay')}
+                            >
+                                STAY
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink 
+                                to="/restaurant" 
+                                className="nav-link" 
+                                activeClassName="active"
+                                onClick={(e) => handleNavLinkClick(e, '/restaurant')}
+                            >
+                                DINE
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink 
+                                to="/contact" 
+                                className="nav-link" 
+                                activeClassName="active"
+                                onClick={(e) => handleNavLinkClick(e, '/contact')}
+                            >
+                                CONTACT
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink 
+                                to="/login" 
+                                className="nav-link" 
+                                activeClassName="active"
+                                onClick={(e) => handleNavLinkClick(e, '/login')}
+                            >
+                                LOGIN
+                            </NavLink>
+                        </li>
+                        {currentWindow.pathname !== '/hotel' && (
+                            <li className="nav-item">
+                                <NavLink 
+                                    to="/hotel" 
+                                    onClick={(e) => handleNavLinkClick(e, '/HotelReserve', true)} // Trigger transition for "BOOK NOW"
+                                    className="btn btn-outline-light" 
+                                    activeClassName="active"
+                                >
+                                    BOOK NOW
+                                </NavLink>
+                            </li>
+                        )}
+                    </ul>
+                </div>
             </div>
-          </div>
-          <div className='container-fluid'>
-          <div className="collapse navbar-collapse justify-content-center">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                  <NavLink 
-                  to="/" 
-                  className="nav-link" 
-                  activeClassName="active"
-                >
-                  HOME
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink 
-                  to="/about" 
-                  className="nav-link" 
-                  activeClassName="active"
-                >
-                  ABOUT
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink 
-                  to="/stay" 
-                  className="nav-link" 
-                  activeClassName="active"
-                >
-                  STAY
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink 
-                  to="/restaurant" 
-                  className="nav-link" 
-                  activeClassName="active"
-                >
-                  DINE
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink 
-                  to="/contact" 
-                  className="nav-link" 
-                  activeClassName="active"
-                >
-                  CONTACT
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink 
-                  to="/login" 
-                  className="nav-link" 
-                  activeClassName="active"
-                >
-                  LOGIN
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          </div>
 
-          {/* Video Transition Overlay */}
-          {isTransitioning && (
-  <div className="transition-wrapper">
-    <div className={`transition-overlay ${slideClass}`}></div>
-    <video
-      id="transition-video"
-      className={`transition-video ${videoClass}`}
-      muted
-      loop={false}
-      autoPlay
-    >
-      <source src={RocketVid} type="video/mp4" />
-    </video>
-  </div>
-)}
-
-
+            {/* Video Transition Overlay */}
+            {isTransitioning && (
+                <div className="transition-wrapper">
+                    <div className={`transition-overlay ${slideClass}`}></div>
+                    <video
+                        id="transition-video"
+                        className={`transition-video ${videoClass}`}
+                        muted
+                        loop={false}
+                        autoPlay
+                    >
+                        <source src={RocketVid} type="video/mp4" />
+                    </video>
+                </div>
+            )}
         </nav>
-    )
+    );
 }
-
